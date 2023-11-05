bc1dd8407640ac0307a07e6dc20fcbcd43e0aca5
usii-004 <usmansmooth004@gmail.com>
2023-11-05 18:04:29 +0100

    refactored conditional taiwind classes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,18 +4,8 @@ import React from 'react'
 const Main = (props) => {
   
   return (
-    <div 
-      className={
-        props.darkMode
-        ? 'py-8 px-2 bg-right-bottom bg-no-repeat bg-auto md:bg-contain bg-hero-black md:py-20 bg-[#282D35] h-screen'
-        : 'py-8 px-2 bg-right-bottom bg-no-repeat bg-auto md:bg-contain bg-hero-white md:py-20 bg-[#ffffff] h-screen'
-      }
-    >
-      <div className={
-        props.darkMode
-        ? 'w-full md:w-[45%] mx-auto text-[#ffffff]'
-        : 'w-full md:w-[45%] mx-auto text-[#2B283A]'
-      }>
+    <div className={`py-8 px-2 bg-right-bottom bg-no-repeat bg-auto md:bg-contain bg-hero-black md:py-20 h-screen ${props.darkMode ? 'bg-[#282D35] bg-hero-black' : 'bg-hero-white bg-[#ffffff]'}`} >
+      <div className={`w-full md:w-[45%] mx-auto ${props.darkMode ? 'text-[#ffffff]' : 'text-[#2B283A]'}`}>
         <h2 className='my-6 text-2xl font-bold md:text-6xl'>Fun facts about React</h2>
         <ul className='ml-6 list-disc marker:text-[#00ABCA] marker:text-[30px] md:text-3xl md:marker:text-[55px] md:mx-16 md:leading-[45px]'>
           <li>Was first released in 2013</li>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,7 @@ import logo from '../img/logo.svg';
 
 const Nav = (props) => {
   return (
-    <div className={
-      props.darkMode 
-      ? 'bg-[#21222A] flex justify-between py-4 shadow-md' 
-      : 'bg-[#ffffff] flex justify-between py-4 shadow-md' 
-    }>
+    <div className={`flex justify-between py-4 shadow-md ${props.darkMode ? 'bg-[#21222A]' : 'bg-[#ffffff]'}`}>
       <div className='flex text-[#00ABCA] md:mx-24'>
         <img 
           src={logo} 
@@ -19,19 +15,8 @@ const Nav = (props) => {
         <p className='text-[#ffffff]'>Light</p>
         <div
           onClick={props.handleClick} 
-          className={
-            props.darkMode
-            ? 'w-8 h-4 mx-2 rounded-md bg-[#f5f5f5] cursor-pointer md:w-12 md:mx-4 md:h-6'
-            : 'w-8 h-4 mx-2 rounded-md bg-[#282D35] cursor-pointer md:w-12 md:mx-4 md:h-6' 
-          }
-        >
-          <div 
-            className={
-              props.darkMode 
-              ? 'float-right w-4 h-4 bg-[#282D35] rounded-full md:w-6 md:h-6'
-              : 'float-leftt w-4 h-4 bg-white rounded-full md:w-6 md:h-6'
-            }
-          ></div>
+          className={`w-8 h-4 mx-2 rounded-md cursor-pointer md:w-12 md:mx-4 md:h-6 ${props.darkMode ? ' bg-[#f5f5f5]' : 'bg-[#282D35]'}`}>
+          <div className={`float-right w-4 h-4 rounded-full md:w-6 md:h-6 ${props.darkMode ? ' bg-[#282D35]' : ' bg-white'}`}></div>
         </div>
         <p className='text-[#21222A]'>Dark</p>
       </div>
